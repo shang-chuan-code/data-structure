37a4d8b55c412a462c34da9a617e56f28be428f6
shangchuan <shangchuan97@163.com>
2025-04-09 23:15:12 +0800

    [CHANGE]增加测试demo

---
+++ include/base.h
@@ -0,0 +1,24 @@
+#ifndef _BASE_H
+#define _BASE_H
+
+/****************头文件包含***************/
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+/****************宏定义***************/
+
+#define S_OK           1
+#define S_FAILED       0
+ 
+
+/****************结构体定义***************/ 
+
+
+/****************函数声明***************/ 
+
+
+#endif
--- main.c
+++ main.c
@@ -1,7 +1,67 @@
-#include <stdio.h>
+#include "base.h"
 #include "list.h"
 
-int main()
+#define MENU_MAX_OUTPUT
+
+char list_menu[MENU_MAX_OUTPUT] = 
 {
-    printf("hello world\n");
+    "\
+    \r\n 0.add head \
+    \r\n 1.add tail \
+    \r\n 2.del head \
+    \r\n 3.add tail \
+    \r\n please input your choose:"\
+
+};
+
+
+static int unit_test_list()
+{
+    char ch;
+    Node* head = NULL;
+    while (1)
+    {
+        printf(list_menu);
+        scanf("%c", &ch);
+        switch(ch)
+        {
+            case '0':
+            {
+                break;
+            }
+            case '1':
+            {
+                break;
+            }
+        }
+        usleep(100* 1000);
+
+    }
+    return S_OK;
+}
+
+
+int main(int argc, char *arg[])
+{
+    int i;
+    for(i=0; i<argc; i++)
+    {
+        printf("arg[%d] = %s\n", i, arg[i]);
+    }
+
+    if(strcmp(arg[1], "list") == 0)
+    {
+        unit_test_list();
+    }
+    else if (strcmp(arg[1], "stack") == 0)
+    {
+
+    }
+    else
+    {
+        unit_test_list();
+    }
+    
+
+    return S_OK;
 }
