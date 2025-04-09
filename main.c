b1866645711e0ff97a017b98701c05557ab7b3a9
shangchuan <shangchuan97@163.com>
2025-04-09 20:42:17 +0800

    [CHANGE]基本框架搭建完成

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
+#include "list.h"
 
 int main()
 {
-
-    
-
+    printf("hello world\n");
 }
---
+++ src/list/list.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "list.h"
+
+static void add_tail(Node** head, int data)
+{
+
+  return;
+}
